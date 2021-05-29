cff2eadfbceb8ea0a0b3f77bd0e0681a17c13cd6
humangasaur <85014500+humangasaur@users.noreply.github.com>
2021-05-29 14:50:03 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+console.log("My first console project");
+
+var first_name ="Vardhman";
+var sec_name ="Jain";
+var full_name= first_name.concat(sec_name);
+console.log(full_name);
+
+var a = 10;
+var b = 5;
+
+var add: a + b;
+console.log(add);
+
+var sub: a - b;
+console.log(sub);
+
+var mul: a * b;
+console.log(mul);
+
+var div: a / b;
+console.log(div);
